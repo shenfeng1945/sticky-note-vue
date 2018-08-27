b0b58eabe43897572777ec1059a7a8a8015d2470
shenfeng1945 <823902826@qq.com>
2018-08-27 19:13:46 +0800

    路由通配

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -7,6 +7,7 @@ let url = {
     editFinish: '/api/notes/finish',
 }
 let host= 'http://127.0.0.1:3000'
+// let host= 'http://shenfeng1945.top:3000'
 for(let key in url){
     if(url.hasOwnProperty(key)){
         url[key] = host + url[key]

c543717080d4d696c14df6409fb4d2b936692d49
shenfeng1945 <823902826@qq.com>
2018-09-24 23:30:33 +0800

    设置跳转路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MainComponent from '@/components/MainComponent'
+import SetAuth from '@/components/SetAuth'
 
 Vue.use(Router)
 
@@ -12,8 +13,13 @@ export default new Router({
       component: MainComponent
     },
     {
-        path: '*',
-        redirect: '/'
+        path: '/setAuth',
+        name: 'SetAuth',
+        component: SetAuth,
+    },
+    {
+      path: '*',
+      redirect: '/'
     }
   ]
 })

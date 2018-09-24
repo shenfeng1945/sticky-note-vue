65e7504ce9c6b8eb8e1399af53e1cb957422ec05
shenfeng1945 <823902826@qq.com>
2018-09-24 23:29:33 +0800

    接口ip修改

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -6,8 +6,7 @@ let url = {
     deleteNote:'/api/notes/delete',
     editFinish: '/api/notes/finish',
 }
-let host= 'http://127.0.0.1:3000'
-// let host= 'http://shenfeng1945.top:3000'
+let host= 'https://shenfeng1945.xyz:6060'
 for(let key in url){
     if(url.hasOwnProperty(key)){
         url[key] = host + url[key]

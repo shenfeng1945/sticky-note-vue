f89e5bf17ca2ad0b42173c25151ec4eed011e795
shenfeng1945 <823902826@qq.com>
2018-09-24 23:28:38 +0800

    接受token，并处理token的中转站

---
+++ src/components/SetAuth.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+export default {
+    created(){
+      const token = this.$router.currentRoute.query.token
+      if(token){
+         this.successLogin(token)
+      }else{
+         this.$router.replace('/')
+      }
+    },
+    methods: {
+        successLogin(token){
+            localStorage.setItem('token',token)
+            this.$router.replace('/')
+        }
+    }
+}
+</script>
+

60324af57e2ec02f0b07073eb17064c22d288c3b
shenfeng1945 <823902826@qq.com>
2023-01-27 00:03:44 +0800

    更改github登陆跳转ip

--- src/components/GithubPage.vue
+++ src/components/GithubPage.vue
@@ -1,7 +1,8 @@
 <template>
    <div>
       <div class="github" v-if="!isAuthorizate">
-        <a href="https://shenfeng1945.cn:6060/auth/github">
+        <a href="http://117.50.172.151:6060/oauth/github">
+        <!-- <a href="https://github.com/login/oauth/authorize?client_id=b90db91c737315789772&redirect_uri=http://117.50.172.151:6060/oauth/redirect"> -->
              <svg class="icon icon-28" style="fill:white;">
                 <use xlink:href="#icon-github"></use>
              </svg>
@@ -18,63 +19,61 @@
    </div>
 </template>
 <script>
-import jwtDecode from 'jwt-decode'
-import axios from 'axios'
+import jwtDecode from "jwt-decode";
+import axios from "axios";
 export default {
-    data(){
-        return {
-           user: {},
-           isAuthorizate: false,
-        }
+  data() {
+    return {
+      user: {},
+      isAuthorizate: false
+    };
+  },
+  created() {
+    if (this.getToken()) {
+      this.user = jwtDecode(this.getToken());
+      this.setAuthorizationToken(this.getToken());
+    }
+    if (this.user.username) {
+      this.$parent.getAllNoteLists();
+    }
+  },
+  methods: {
+    getToken() {
+      return localStorage.getItem("token");
     },
-    created(){
-       if(this.getToken()){
-         this.user = jwtDecode(this.getToken())
-         this.setAuthorizationToken(this.getToken())
-       }
-       if(this.user.username){
-          this.$parent.getAllNoteLists()
-       }
+    setAuthorizationToken(jwtToken) {
+      if (jwtToken) {
+        this.isAuthorizate = true;
+        axios.defaults.headers.common["Authorization"] = `Bearer ${jwtToken}`;
+      } else {
+        this.isAuthorizate = false;
+        delete axios.defaults.headers.common["Authorization"];
+      }
     },
-    methods: {
-        getToken(){
-           return localStorage.getItem('token')    
-        },
-        setAuthorizationToken(jwtToken){
-            if(jwtToken){
-                this.isAuthorizate = true;
-                axios.defaults.headers.common['Authorization'] = `Bearer ${jwtToken}`;
-            }else{
-                this.isAuthorizate = false;
-                delete axios.defaults.headers.common['Authorization'];
-            }
-        },
-        logout(){
-            localStorage.removeItem('token');
-            this.setAuthorizationToken(false)
-            this.$parent.getNoteLists();
-        }
-    },
-}
+    logout() {
+      localStorage.removeItem("token");
+      this.setAuthorizationToken(false);
+      this.$parent.getNoteLists();
+    }
+  }
+};
 </script>
 
 <style scoped>
-  .isLogin{
-      display: flex;
-      align-items: center;
-  }
-  .logout{
-      margin-left: 10px;
-      color: #00d3aa;
-      cursor: pointer;
-  }
-  .avatar{
-      width:36px;
-      height: 36px;
-      vertical-align: middle;
-      border-radius: 50%;
-      margin-right:5px;
-  }
-
+.isLogin {
+  display: flex;
+  align-items: center;
+}
+.logout {
+  margin-left: 10px;
+  color: #00d3aa;
+  cursor: pointer;
+}
+.avatar {
+  width: 36px;
+  height: 36px;
+  vertical-align: middle;
+  border-radius: 50%;
+  margin-right: 5px;
+}
 </style>
-

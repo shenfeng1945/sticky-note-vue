88bb4a405aa6d4cbed9f38c54e50eafdabd864bd
shenfeng1945 <823902826@qq.com>
2023-01-27 00:05:46 +0800

    修改ip

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -1,15 +1,16 @@
 let url = {
-    articleLists: '/api/notes',
-    starEdit: '/api/notes/editStar',
-    articleEdit: '/api/notes/edit',
-    addNote: '/api/notes/add',
-    deleteNote:'/api/notes/delete',
-    editFinish: '/api/notes/finish',
+  articleLists: "/api/notes",
+  starEdit: "/api/notes/editStar",
+  articleEdit: "/api/notes/edit",
+  addNote: "/api/notes/add",
+  deleteNote: "/api/notes/delete",
+  editFinish: "/api/notes/finish"
+};
+// let host= 'https://shenfeng1945.cn:6060'
+let host = "http://117.50.172.151:6060";
+for (let key in url) {
+  if (url.hasOwnProperty(key)) {
+    url[key] = host + url[key];
+  }
 }
-let host= 'https://shenfeng1945.cn:6060'
-for(let key in url){
-    if(url.hasOwnProperty(key)){
-        url[key] = host + url[key]
-    }
-}
-export default url
+export default url;
